a68255ed056e3f2f993ee1515abc5e4dbaf37ada
Raymond <raymondcheesheng@gmail.com>
2019-04-05 22:54:41 +0800

    ok

---
+++ src/main.js
@@ -0,0 +1,29 @@
+function toggleFunction(mainClickName, toggleIconName) {
+	document.querySelectorAll(mainClickName).forEach(function(link) {
+		link.addEventListener('click', function(changeIcon) {
+			document.querySelectorAll(toggleIconName).forEach(function(subLink) {
+				subLink.classList.toggle('fa-toggle-off');
+				subLink.classList.toggle('fa-toggle-on');
+			});
+		});
+	});
+}
+function viewAll() {
+	let view = document.getElementById('newBooking');
+	let viewAll = document.getElementById('newBookingAll');
+	if (view.style.display == 'none') {
+		viewAll.classList.remove('d-none');
+	} else {
+		view.style.display = 'none';
+		viewAll.classList.remove('d-none');
+	}
+}
+function view() {
+	let view = document.getElementById('newBooking');
+	let viewAll = document.getElementById('newBookingAll');
+	if (view.style.display == 'none') {
+		viewAll.classList.add('d-none');
+		view.style.display = 'block';
+	} else {
+	}
+}
---
+++ src/router.js
@@ -0,0 +1,12 @@
+//home router
+$('#newBooking').load('../home/home_new_booking.html');
+$('#newBookingAll').load('../home/home_new_booking_all.html');
+//customer booking router
+$('#dispatched').load('../customerBooking/dispatched_booking.html');
+$('#finished').load('../customerBooking/finished_booking.html');
+$('#cancelled').load('../customerBooking/cancel_booking.html');
+//staff
+$('#manageStaff').load('../staff/manage_staff.html');
+$('#manageLeave').load('../staff/manage_leave.html');
+$('#advancePayment').load('../staff/advance_payment.html');
+$('#payout').load('../staff/payout.html');
